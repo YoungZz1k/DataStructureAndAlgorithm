0a819ceb4b1adfc00e30ab5a05a3352da14811cd
YoungZz1k <907175262@qq.com>
2023-11-29 19:37:11 +0800

    2023/11/29  使用有序数组实现优先队列

---
+++ src/main/java/com/YoungZz1k/DataStructure/Queue/PriorityQueue/PriorityQueue2.java
@@ -0,0 +1,76 @@
+package com.YoungZz1k.DataStructure.Queue.PriorityQueue;
+
+import com.YoungZz1k.DataStructure.Queue.Queue;
+
+/**
+ * 基于有序数组实现的优先队列
+ * @param <E>
+ */
+public class PriorityQueue2<E extends Priority> implements Queue<E> {
+
+
+    private Priority[] array;
+    private int size;
+
+    public PriorityQueue2(int capacity) {
+        array = new Priority[capacity];
+    }
+
+    @Override
+    public boolean offer(E value) {
+        if(isFull()){
+            return false;
+        }
+        insert(value);
+        size++;
+        return true;
+    }
+
+
+    private void insert(E e){
+        int i = size - 1;
+        while (i >= 0 && array[i].priority() > e.priority()){
+            array[i + 1] = array[i];
+            i--;
+        }
+        array[i + 1] = e;
+    }
+
+    @Override
+    public E pull() {
+        if (isEmpty()){
+            return null;
+        }
+        E e =(E) array[size - 1];
+        size--;
+        array[size] = null;// 引用地址置为null
+        return e;
+    }
+
+    private void remove(int index) {
+        if (index < size - 1){
+            // 移动
+            System.arraycopy(array,index + 1,array,index,size - 1 -index);
+        }
+        size--;
+    }
+
+    @Override
+    public E peek() {
+        if (isEmpty()){
+            return null;
+        }
+        return (E) array[size - 1];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean isFull() {
+        return size == array.length;
+    }
+
+}
