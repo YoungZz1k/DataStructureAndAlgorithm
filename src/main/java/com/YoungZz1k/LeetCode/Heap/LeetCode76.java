204caa844825ab22a342f17b1c9a7c90372428f7
YoungZz1k <907175262@qq.com>
2023-11-30 23:27:14 +0800

    2023/11/30  lc76求数组第k大元素（小顶堆实现）

--- src/main/java/com/YoungZz1k/LeetCode/Heap/LeetCode76.java
+++ src/main/java/com/YoungZz1k/LeetCode/Heap/LeetCode76.java
@@ -1,6 +1,7 @@
 package com.YoungZz1k.LeetCode.Heap;
 
 import com.YoungZz1k.DataStructure.Heap.MaxHeap;
+import com.YoungZz1k.DataStructure.Heap.MinHeap;
 
 /**
  * 找数组内第k大的元素
@@ -25,11 +26,31 @@ public class LeetCode76 {
         return num;
     }
 
+    /**
+     * 小顶堆实现
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int findKthLargest2(int[] nums, int k) {
+        MinHeap heap = new MinHeap(k);
+        for (int i = 0; i < k; i++) {
+            heap.offer(nums[i]);
+        }
+        for (int i = k; i < nums.length; i ++){
+            if (nums[i] > heap.peek()){
+                heap.replace(nums[i]);
+            }
+        }
+        return heap.peek();
+    }
+
 
 
     public static void main(String[] args) {
         int[] array = {1,5,3,7,9};
+        int[] array1 = {3,2,3,1,2,4,5,5,6};
         int k = 2;
-        System.out.println(findKthLargest1(array, 2));
+        System.out.println(findKthLargest2(array1, 4));
     }
 }
