456f7b3cd245d68f2e180f91d2bf27fb9ad46b72
YoungZz1k <907175262@qq.com>
2023-12-03 00:00:36 +0800

    2023/12/02  二叉树前中序遍历（非递归）

--- src/main/java/com/YoungZz1k/DataStructure/BinaryTree/TreeTraversal.java
+++ src/main/java/com/YoungZz1k/DataStructure/BinaryTree/TreeTraversal.java
@@ -2,6 +2,8 @@ package com.YoungZz1k.DataStructure.BinaryTree;
 
 import sun.reflect.generics.tree.Tree;
 
+import java.util.LinkedList;
+
 /**
  * 树的遍历
  */
@@ -47,6 +49,26 @@ public class TreeTraversal {
         System.out.printf(node.val + "\t");// 值
     }
 
+    /**
+     * 前序遍历(非递归)
+     * @param node
+     */
+    static void preOrder1(TreeNode node){
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode curr = node;
+        while (curr != null || !stack.isEmpty()){
+            if (curr != null){
+                System.out.println("去 : " + curr.val);// 前序遍历
+                stack.push(curr);// 压入走过的节点，方便回溯
+                curr = curr.left;//
+            }else{
+                TreeNode pop = stack.pop();// 弹栈刚好是中序遍历
+//                System.out.println("回 : " + pop.val);
+                curr = pop.right;// 左边为空时 取出上一步的右孩子赋给curr
+            }
+        }
+    }
+
     //       1
     //      / \
     //     2   3
@@ -58,13 +80,14 @@ public class TreeTraversal {
                 1,
                 new TreeNode(new TreeNode(5),3,new TreeNode(6))
         );
-        // 前序遍历 1 2 4 3 5 6
-        preOrder(treeNode);
-        System.out.println();
-        // 中序遍历 4 2 1 5 3 6
-        inOrder(treeNode);
-        System.out.println();
-        // 后序遍历 4 2 5 6 3 1
-        postOrder(treeNode);
+//        // 前序遍历 1 2 4 3 5 6
+//        preOrder(treeNode);
+//        System.out.println();
+//        // 中序遍历 4 2 1 5 3 6
+//        inOrder(treeNode);
+//        System.out.println();
+//        // 后序遍历 4 2 5 6 3 1
+//        postOrder(treeNode);
+        preOrder1(treeNode);
     }
 }
