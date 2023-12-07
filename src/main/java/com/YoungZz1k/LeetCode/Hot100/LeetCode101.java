56598e8e3e39b1a5344ea6e505ee79934a16d8a6
YoungZz1k <907175262@qq.com>
2023-12-07 14:16:13 +0800

    2023/12/07 lc101对称二叉树

---
+++ src/main/java/com/YoungZz1k/LeetCode/Hot100/LeetCode101.java
@@ -0,0 +1,25 @@
+package com.YoungZz1k.LeetCode.Hot100;
+
+import com.YoungZz1k.DataStructure.BinaryTree.TreeNode;
+
+/**
+ * 对称二叉树
+ */
+public class LeetCode101 {
+    public boolean isSymmetric(TreeNode root) {
+        return check(root.left,root.right);
+    }
+
+    private boolean check(TreeNode left, TreeNode right) {
+        if (left == null && right == null){
+            return true;
+        }
+        if ((right == null) || (left == null)){
+            return false;
+        }
+        if (left.val != right.val){
+            return false;
+        }
+        return check(left.left, right.right) && check(left.right, right.left);
+    }
+}
