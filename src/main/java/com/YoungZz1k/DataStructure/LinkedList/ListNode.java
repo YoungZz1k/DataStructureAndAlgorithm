a8e60fe58444979c0a629771b600e7f5a731501e
YoungZz1k <907175262@qq.com>
2023-12-15 21:26:28 +0800

    2023/12/15 链表节点类更新

--- src/main/java/com/YoungZz1k/DataStructure/LinkedList/ListNode.java
+++ src/main/java/com/YoungZz1k/DataStructure/LinkedList/ListNode.java
@@ -9,6 +9,10 @@ public class ListNode {
 
     public ListNode next;
 
+    public ListNode(int val) {
+        this.val = val;
+    }
+
     public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
