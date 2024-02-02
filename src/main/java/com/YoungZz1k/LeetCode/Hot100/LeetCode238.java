5500000ae87cdda461c7cfbc525d79626abb456f
YoungZz1k <907175262@qq.com>
2024-02-02 13:41:08 +0800

    2024/02/02 lc238除自身以外数组的乘积

---
+++ src/main/java/com/YoungZz1k/LeetCode/Hot100/LeetCode238.java
@@ -0,0 +1,64 @@
+package com.YoungZz1k.LeetCode.Hot100;
+
+/**
+ * 除自身以外数组的乘积
+ */
+public class LeetCode238 {
+
+    /**
+     * 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+     * 题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+     * 请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
+     * 示例 1:
+     * 输入: nums = [1,2,3,4]
+     * 输出: [24,12,8,6]
+     * 示例 2:
+     * 输入: nums = [-1,1,0,-3,3]
+     * 输出: [0,0,9,0,0]
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[nums.length];
+        int[] left = new int[n];// 左侧所有元素乘积
+        int[] right = new int[n];// 右侧所有元素乘积
+        left[0] = 1;
+        right[n - 1] = 1;
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            res[i] = left[i] * right[i];
+        }
+        return res;
+    }
+
+    // O(n2)
+    public int[] productExceptSelf1(int[] nums) {
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 1;
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j) {
+                    continue;
+                } else {
+                    sum *= nums[j];
+                    res[i] = sum;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        LeetCode238 leetCode238 = new LeetCode238();
+        int[] ints = leetCode238.productExceptSelf(new int[]{-1,1,0,-3,3});
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+    }
+}
